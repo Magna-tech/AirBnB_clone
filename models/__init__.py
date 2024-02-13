72b593cc5f24c89477e04b3d5c7ce8ec43d6127b
Nathan Omeri <magembenath@gmail.com>
2024-02-13 18:42:29 +0000

    Console update

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,16 @@
 """
 initialize the models package
 """
+
+from os import getenv
+
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
